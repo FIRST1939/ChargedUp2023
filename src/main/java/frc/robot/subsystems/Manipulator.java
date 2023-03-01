5e761384be1722b4e1fb3e0f7f94d9f01ad33d5e
StarbuckBarista <carlitos.salazar@protonmail.com>
2023-02-28 21:02:38 -0600

    Removed Unnecessary Import

--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.commands.ZeroArm;
 
 public class Manipulator extends SubsystemBase {
     
